7739d3b52285de067494a10f328feb4dfe97c788
Othmane ElAlaoui Banouzi <lateg15@gmail.com>
2020-04-23 22:16:20 +0000

    build: added gh pages semantic plugin

--- release.config.js
+++ release.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   pkgRoot: "dist/mat-advanced-table",
   tarballDir: "dist/",
+  branch: "master",
+  verifyConditions: [
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    "@qiwi/semantic-release-gh-pages-plugin",
+  ],
+  publish: [
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@qiwi/semantic-release-gh-pages-plugin",
+      {
+        msg: "deploy gh-pages",
+        src: "dist/mat-advanced-table-examples",
+      },
+    ],
+  ],
 };
