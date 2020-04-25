7b6789a1a86804a3afd4d5896e6153e7ae656b8a
Othmane ElAlaoui Banouzi <lateg15@gmail.com>
2020-04-25 17:45:02 +0000

    test(normalized-pipe): fixed tests

--- projects/mat-advanced-table/src/lib/normalize-decimal.pipe.spec.ts
+++ projects/mat-advanced-table/src/lib/normalize-decimal.pipe.spec.ts
@@ -1,28 +1,27 @@
 import { NormalizeDecimalPipe } from "./normalize-decimal.pipe";
 
 describe("NormalizeDecimalPipe", () => {
+  let pipe: NormalizeDecimalPipe;
+  beforeEach(() => {
+    pipe = new NormalizeDecimalPipe();
+  });
   it("create an instance", () => {
-    const pipe = new NormalizeDecimalPipe();
     expect(pipe).toBeTruthy();
   });
   it("removes comma separators from decimal literals", () => {
-    const pipe = new NormalizeDecimalPipe();
-    expect(pipe.transform("1,210,322.21")).toEqual("112032221");
-    expect(pipe.transform(",322")).toEqual("322");
+    expect(pipe.transform("1,210,322.21")).toBe("1210322.21");
+    expect(pipe.transform(",322")).toBe("322");
   });
   it("returns 0 when null or undefined", () => {
-    const pipe = new NormalizeDecimalPipe();
-    expect(pipe.transform(null)).toEqual(0);
-    expect(pipe.transform(undefined)).toEqual(0);
+    expect(pipe.transform(null)).toBe("0");
+    expect(pipe.transform(undefined)).toBe("0");
   });
   it("returns 0 when non number is passed ", () => {
-    const pipe = new NormalizeDecimalPipe();
-    const literal = "121121.232";
-    expect(pipe.transform(literal)).toEqual(literal);
+    const literal = "not a number";
+    expect(pipe.transform(literal)).toBe("0");
   });
   it("returns same literal when no comma is included", () => {
-    const pipe = new NormalizeDecimalPipe();
     const literal = "121121.232";
-    expect(pipe.transform(literal)).toEqual(literal);
+    expect(pipe.transform(literal)).toBe(literal);
   });
 });
